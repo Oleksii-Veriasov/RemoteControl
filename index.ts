c8152841b0f4ddecb449f2315d214be2a32d8333
Oleksii <alexv77ua@gmail.com>
2022-06-26 16:38:25 +0300

    add take screenshot

--- index.ts
+++ index.ts
@@ -1,69 +1,88 @@
-import Jimp from "jimp";
 import { httpServer } from "./src/http_server/index";
 import robot from "robotjs";
-import { WebSocketServer } from "ws";
+import { createWebSocketStream, WebSocketServer } from "ws";
 import "dotenv/config";
 import { drawCircle } from "./src/drawCircle";
 import { drawRectangle } from "./src/drawRectangle";
+import { makeScreenshot } from "./src/makeScreensot";
 const PORT = process.env.PORT;
-
 const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
 const server = new WebSocketServer({ port: parseInt(`${PORT}`) });
+try {
+  server.on("connection", (socket: any) => {
+    const duplex = createWebSocketStream(socket, {
+      encoding: "utf8",
+      decodeStrings: false,
+    });
+    duplex.on("data", async (data: Buffer) => {
+      let comandLine = data.toString("utf-8").split(" ");
+      let [comand, ...args] = comandLine;
+      const { x, y } = robot.getMousePos();
 
-server.on("connection", (socket: any) => {
-  socket.on("message", (data: Buffer) => {
-    let comandLine = data.toString("utf-8").split(" ");
-    console.log(comandLine);
-    let [comand, ...args] = comandLine;
-    console.log(args);
-    const { x, y } = robot.getMousePos();
-    console.log(x, y);
-    switch (comand) {
-      case "mouse_up":
-        console.log(`->-${comand} {${args}}`);
-        console.log(args[0]);
-        console.log(+x + parseInt(args[0]));
-        socket.send(`${comand}\0`);
-        robot.moveMouseSmooth(x, y - parseInt(args[0]));
-        break;
-      case "mouse_down":
-        console.log(`->-${comand} {${args}}`);
-        socket.send(`${comand}\0`);
-        robot.moveMouse(x, y + parseInt(args[0]));
-        break;
-      case "mouse_left":
-        console.log(`->-${comand}{${args} px}\0`);
-        socket.send(`${comand} ${args}\0`);
-        robot.moveMouse(x - parseInt(args[0]), y);
-        break;
-      case "mouse_right":
-        console.log(`->-${comand}{${args} px}\0`);
-        socket.send(`${comand}\0`);
-        robot.moveMouse(x + parseInt(args[0]), y);
-        break;
-      case "mouse_position":
-        console.log(`->-${comand}`);
-        socket.send(`${comand} ${x},${y}\0`);
-        break;
-      case "draw_circle":
-        console.log(`->-${comand}`);
-        drawCircle(parseInt(args[0]));
-        socket.send(`${comand}\0`);
-        break;
-      case "draw_rectangle":
-        console.log(`->-${comand}`);
-        drawRectangle(parseInt(args[0]), parseInt(args[1]));
-        socket.send(`${comand}\0`);
-        break;
-      case "draw_square":
-        console.log(`->-${comand}`);
-        drawRectangle(parseInt(args[0]), parseInt(args[0]));
-        socket.send(`${comand}\0`);
-        break;
-    }
-  }); 
-});
+      switch (comand) {
+        case "mouse_up":
+          console.log(`->-${comand} {${args}}`);
+          duplex.write(`${comand}\0`);
+          robot.moveMouseSmooth(x, y - parseInt(args[0]));
+          break;
+        case "mouse_down":
+          console.log(`->-${comand} {${args}}`);
+          duplex.write(`${comand}\0`);
+          robot.moveMouse(x, y + parseInt(args[0]));
+          break;
+        case "mouse_left":
+          console.log(`->-${comand}{${args} px}\0`);
+          duplex.write(`${comand} ${args}\0`);
+          robot.moveMouse(x - parseInt(args[0]), y);
+          break;
+        case "mouse_right":
+          console.log(`->-${comand}{${args} px}\0`);
+          duplex.write(`${comand}\0`);
+          robot.moveMouse(x + parseInt(args[0]), y);
+          break;
+        case "mouse_position":
+          console.log(`->-${comand}`);
+          duplex.write(`${comand} ${x},${y}\0`);
+          break;
+        case "draw_circle":
+          console.log(`->-${comand}`);
+          drawCircle(parseInt(args[0]));
+          duplex.write(`${comand}\0`);
+          break;
+        case "draw_rectangle":
+          console.log(`->-${comand}`);
+          drawRectangle(parseInt(args[0]), parseInt(args[1]));
+          duplex.write(`${comand}\0`);
+          break;
+        case "draw_square":
+          console.log(`->-${comand}`);
+          drawRectangle(parseInt(args[0]), parseInt(args[0]));
+          duplex.write(`${comand}\0`);
+          break;
+        case "prnt_scrn":
+          console.log(`->-${comand}`);
+          const img = await makeScreenshot();
+          duplex.write(`${comand} ${img}`);
+          break;
+      }
+    });
+  });
+  server.on("close", (socket: any) => {
+    socket.terminate();
+    server.close();
+    process.exit(0);
+  });
+  process.on("SIGINT", () => {
+    process.stdout.write("Closing websocket...\n");
+    server.close();
+    process.exit(0);
+  });
+} catch (ex) {
+  console.log(ex);
+  server.close();
+  process.exit(0);
+}
--- src/drawRectangle.ts
+++ src/drawRectangle.ts
@@ -6,25 +6,18 @@ export const drawRectangle = async (width: number, hight: number) => {
   robot.mouseToggle("down", "left");
   for (let i = 0; i <= parts; i++) {
     let { x, y } = robot.getMousePos();
-    console.log(x + width / parts, y);
     robot.dragMouse(x + width / parts, y);
   }
   for (let i = 0; i <= parts; i++) {
-    const mousePos = robot.getMousePos();
     let { x, y } = robot.getMousePos();
-    console.log(x, y + hight / parts);
     robot.dragMouse(x, y + hight / parts);
   }
   for (let i = 0; i <= parts; i++) {
-    const mousePos = robot.getMousePos();
     let { x, y } = robot.getMousePos();
-    console.log(x - width / parts, y);
     robot.dragMouse(x - width / parts, y);
   }
   for (let i = 0; i <= parts; i += 1) {
-    const mousePos = robot.getMousePos();
     let { x, y } = robot.getMousePos();
-    console.log(x - width / parts, y - hight / parts);
     robot.dragMouse(x, y - hight / parts);
   }
   robot.mouseToggle("up", "left");
